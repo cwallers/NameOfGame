dd4deaae8e4aaf11f961edb2ec564bf3bbe2b7b7
Paul Toillion <ptoillion@harding.edu>
2017-02-11 17:52:46 -0600

    Main Menu/Game Board up

---
+++ Pastry Pandemonium/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour {
+
+    public GameObject current;
+
+
+    public void OnMouseEnter()
+    {
+        LeanTween.scale(current, new Vector3(.45f, .45f, .45f), .3f);
+    }
+
+    public void OnMouseUp()
+    {
+        if (current.name == "singlePlayer")
+        {
+            SceneManager.LoadScene("GameBoard");
+        }
+    }
+    public void OnMouseExit()
+    {
+        LeanTween.scale(current, new Vector3(0.3615471f, 0.3615471f, 0.3615471f), .075f);
+
+    }
+
+}
