551a39fffc5124beda8bb644fe82bfc8fcda4146
Paul Toillion <ptoillion@harding.edu>
2017-02-16 16:51:57 -0600

    Singleplayer menu stuff

--- Pastry Pandemonium/Assets/Scripts/MainMenuButton.cs
+++ Pastry Pandemonium/Assets/Scripts/MainMenuButton.cs
@@ -7,30 +7,47 @@ public class MainMenuButton : MonoBehaviour {
 
     public GameObject current;
     public GameObject canvas;
+	private GameObject multiplayer, tutorial, options, singleplayer;
 
 
+	private void Awake()
+	{
+		multiplayer = GameObject.FindGameObjectWithTag("multiplayer");
+		tutorial = GameObject.FindGameObjectWithTag("tutorial");
+		options = GameObject.FindGameObjectWithTag("options");
+		singleplayer = GameObject.FindGameObjectWithTag("singleplayer");
+	}
     public void OnMouseEnter()
     {
+		//Scales menu options to indicate that you can click on them
         LeanTween.scale(current, new Vector3(.45f, .45f, .45f), .075f);
     }
+	public void OnMouseExit()
+	{
+		//Sets menu options back to their original size
+		LeanTween.scale(current, new Vector3(0.3615471f, 0.3615471f, 0.3615471f), .05f);
+	}
 
     public void OnMouseUp()
     {
+
+		//Loads the singleplayer option menu
         if (current.name == "singlePlayer")
         {
             //SceneManager.LoadScene("GameBoard");
             canvas.SetActive(true);
+			multiplayer.SetActive(false);
+			tutorial.SetActive (false);
+			options.SetActive (false);
             
         }
+
+		//loads multiplayer scene
         if (current.name == "multiplayer")
         {
             SceneManager.LoadScene("Multiplayer");
         }
     }
-    public void OnMouseExit()
-    {
-        LeanTween.scale(current, new Vector3(0.3615471f, 0.3615471f, 0.3615471f), .05f);
 
-    }
 
 }
--- Pastry Pandemonium/Assets/Scripts/SinglePlayerMenu.cs
+++ Pastry Pandemonium/Assets/Scripts/SinglePlayerMenu.cs
@@ -7,9 +7,11 @@ public class SinglePlayerMenu : MonoBehaviour {
 
     public GameObject current, character;
     private GameObject moveAi, moveUser, difficultyNormal, difficultyHard, menu, multiplayer, tutorial, options;
+	private GameObject[] players;
 
     public void OnMouseEnter()
     {
+		//Scales objects to indicate you can click on them
         LeanTween.scale(current, new Vector3(0.5f, .5f, .5f), .075f);
 		LeanTween.scale(character, new Vector3(0.3f, .3f, .3f), .03f);
 	}
@@ -17,74 +19,91 @@ public class SinglePlayerMenu : MonoBehaviour {
 
 	public void OnMouseExit()
 	{
+		//Sets objects back to their original size
 		LeanTween.scale(current, new Vector3(0.4268945f, 0.4268945f, 0.4268945f), .05f);
-	LeanTween.scale(character, new Vector3(0.222469f, 0.222469f, 0.222469f), .01f);
+		LeanTween.scale(character, new Vector3(0.222469f, 0.222469f, 0.222469f), .01f);
 
 
 	}
 
     private void Awake()
     {
+		//Initiating all the game objects needed
         moveUser = GameObject.Find("userSelected");
         moveAi = GameObject.Find("aiSelected");
         difficultyNormal = GameObject.Find("normalSelected");
         difficultyHard = GameObject.Find("hardSelected");
         menu = GameObject.Find("Popup");
+		multiplayer = GameObject.FindGameObjectWithTag("multiplayer");
+		tutorial = GameObject.FindGameObjectWithTag("tutorial");
+		options = GameObject.FindGameObjectWithTag("options");
 
-
-		multiplayer = GameObject.Find("multiplayer");
-		tutorial = GameObject.Find("tutorial");
-		options = GameObject.Find("options");
-		multiplayer.GetComponent<Renderer>().enabled = false;
-		tutorial.GetComponent<Renderer>().enabled = false;
-		options.GetComponent<Renderer>().enabled = false;
-
-
+		players = GameObject.FindGameObjectsWithTag ("Player");
+		for(int i = 0; i < players.Length; i++)
+		{
+			if (players [i].name != "redCupcake") {
+				LeanTween.alpha (players [i], 0.35f, 0f);
+			}
+		}
+			
+		//Set selection defaults (AI, redCupcake, Normal) 
         difficultyNormal.GetComponent<Renderer>().enabled = true;
         difficultyHard.GetComponent<Renderer>().enabled = false;
-        moveAi.GetComponent<Renderer>().enabled = true;
-        moveUser.GetComponent<Renderer>().enabled = false;
+        moveAi.GetComponent<Renderer>().enabled = false;
+        moveUser.GetComponent<Renderer>().enabled = true;
+
 
     }
 
-
-
     public void OnMouseUp()
     {
+		
+		//Finds what option you clicked on
 		if (current != null) {
-			switch (current.name) {
-			case "startButton":
-				SceneManager.LoadScene ("GameBoard");
-				break;
-			case "aiButton":
-				moveAi.GetComponent<Renderer> ().enabled = true;
-				moveUser.GetComponent<Renderer> ().enabled = false;
-				break;
-			case "userButton":
-				moveAi.GetComponent<Renderer> ().enabled = false;
-				moveUser.GetComponent<Renderer> ().enabled = true;
-				break;
-			case "normalButton":
-				difficultyNormal.GetComponent<Renderer> ().enabled = true;
-				difficultyHard.GetComponent<Renderer> ().enabled = false;
-				break;
-			case "hardButton":
-				difficultyNormal.GetComponent<Renderer> ().enabled = false;
-				difficultyHard.GetComponent<Renderer> ().enabled = true;
-				break;
-			case "menuButton":
-				menu.SetActive (false);
-				multiplayer.GetComponent<Renderer>().enabled = true;
-				tutorial.GetComponent<Renderer>().enabled = true;
-				options.GetComponent<Renderer>().enabled = true;
-				break;
-			default:
-				break;
-            
+			switch (current.name) 
+			{
+				case "startButton":
+					SceneManager.LoadScene ("GameBoard");
+					break;
+				case "aiButton":
+					moveAi.GetComponent<Renderer> ().enabled = true;
+					moveUser.GetComponent<Renderer> ().enabled = false;
+					break;
+				case "userButton":
+					moveAi.GetComponent<Renderer> ().enabled = false;
+					moveUser.GetComponent<Renderer> ().enabled = true;
+					break;
+				case "normalButton":
+					difficultyNormal.GetComponent<Renderer> ().enabled = true;
+					difficultyHard.GetComponent<Renderer> ().enabled = false;
+					break;
+				case "hardButton":
+					difficultyNormal.GetComponent<Renderer> ().enabled = false;
+					difficultyHard.GetComponent<Renderer> ().enabled = true;
+					break;
+				case "menuButton":
+					menu.SetActive (false);
+					multiplayer.SetActive(true);
+					tutorial.SetActive (true);
+					options.SetActive (true);
+					break;
+				default:
+					break;          
+			}
+		}
 
+
+		//Finds what character you clicked on
+		if (character != null) 
+		{
+			//resets all characters to loo unselected
+			for(int i = 0; i < players.Length; i++)
+			{
+				LeanTween.alpha (players[i], 0.35f, 0f);
 			}
 
-		}
-  
+			//sets the character selected to look selected
+			LeanTween.alpha (character, 1f, .5f);
+		} 
     }
 }
