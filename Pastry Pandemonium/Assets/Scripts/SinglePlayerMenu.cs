d20060a9b7e3fcd2eae2377eed4df3eef08c9077
Paul Toillion <ptoillion@harding.edu>
2017-02-16 00:42:37 -0600

    More pop up menu stuff/fixed bugs

--- Pastry Pandemonium/Assets/Scripts/SinglePlayerMenu.cs
+++ Pastry Pandemonium/Assets/Scripts/SinglePlayerMenu.cs
@@ -5,13 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SinglePlayerMenu : MonoBehaviour {
 
-    public GameObject current;
-    private GameObject moveAi, moveUser, difficultyNormal, difficultyHard, menu;
+    public GameObject current, character;
+    private GameObject moveAi, moveUser, difficultyNormal, difficultyHard, menu, multiplayer, tutorial, options;
 
     public void OnMouseEnter()
     {
         LeanTween.scale(current, new Vector3(0.5f, .5f, .5f), .075f);
-    }
+		LeanTween.scale(character, new Vector3(0.3f, .3f, .3f), .03f);
+	}
+    
+
+	public void OnMouseExit()
+	{
+		LeanTween.scale(current, new Vector3(0.4268945f, 0.4268945f, 0.4268945f), .05f);
+	LeanTween.scale(character, new Vector3(0.222469f, 0.222469f, 0.222469f), .01f);
+
+
+	}
 
     private void Awake()
     {
@@ -22,6 +32,14 @@ public class SinglePlayerMenu : MonoBehaviour {
         menu = GameObject.Find("Popup");
 
 
+		multiplayer = GameObject.Find("multiplayer");
+		tutorial = GameObject.Find("tutorial");
+		options = GameObject.Find("options");
+		multiplayer.GetComponent<Renderer>().enabled = false;
+		tutorial.GetComponent<Renderer>().enabled = false;
+		options.GetComponent<Renderer>().enabled = false;
+
+
         difficultyNormal.GetComponent<Renderer>().enabled = true;
         difficultyHard.GetComponent<Renderer>().enabled = false;
         moveAi.GetComponent<Renderer>().enabled = true;
@@ -29,46 +47,44 @@ public class SinglePlayerMenu : MonoBehaviour {
 
     }
 
-    public void OnMouseExit()
-    {
-        LeanTween.scale(current, new Vector3(0.4268945f, 0.4268945f, 0.4268945f), .05f);
-
-    }
 
 
     public void OnMouseUp()
     {
-        string name = current.name;
-        switch (name)
-        {
-            case "startButton" :
-                SceneManager.LoadScene("GameBoard");
-                break;
-            case "aiButton":
-                moveAi.GetComponent<Renderer>().enabled = true;
-                moveUser.GetComponent<Renderer>().enabled = false;
-                break;
-            case "userButton":
-                moveAi.GetComponent<Renderer>().enabled = false;
-                moveUser.GetComponent<Renderer>().enabled = true;
-                break;
-            case "normalButton":
-                difficultyNormal.GetComponent<Renderer>().enabled = true;
-                difficultyHard.GetComponent<Renderer>().enabled = false;
-                break;
-            case "hardButton":
-                difficultyNormal.GetComponent<Renderer>().enabled = false;
-                difficultyHard.GetComponent<Renderer>().enabled = true;
-                break;
-            case "menuButton":
-                menu.SetActive(false);
-                break;
-            default:
-                break;
+		if (current != null) {
+			switch (current.name) {
+			case "startButton":
+				SceneManager.LoadScene ("GameBoard");
+				break;
+			case "aiButton":
+				moveAi.GetComponent<Renderer> ().enabled = true;
+				moveUser.GetComponent<Renderer> ().enabled = false;
+				break;
+			case "userButton":
+				moveAi.GetComponent<Renderer> ().enabled = false;
+				moveUser.GetComponent<Renderer> ().enabled = true;
+				break;
+			case "normalButton":
+				difficultyNormal.GetComponent<Renderer> ().enabled = true;
+				difficultyHard.GetComponent<Renderer> ().enabled = false;
+				break;
+			case "hardButton":
+				difficultyNormal.GetComponent<Renderer> ().enabled = false;
+				difficultyHard.GetComponent<Renderer> ().enabled = true;
+				break;
+			case "menuButton":
+				menu.SetActive (false);
+				multiplayer.GetComponent<Renderer>().enabled = true;
+				tutorial.GetComponent<Renderer>().enabled = true;
+				options.GetComponent<Renderer>().enabled = true;
+				break;
+			default:
+				break;
             
 
-        }
+			}
 
+		}
   
     }
 }
